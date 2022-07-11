e0129a8cf13c7a83d81e4f031037301d898ba329
Lucas Ricci <llricci@protonmail.com>
2022-07-11 14:10:56 -0300

    Update pop-os-postinstall.sh

--- pop-os-postinstall.sh
+++ pop-os-postinstall.sh
@@ -1,2 +1,73 @@
 #!/usr/bin/env bash
 
+VERMELHO='\e[1;91m'
+VERDE='\e[1;92m'
+SEM_COR='\e[0m'
+
+#FUNÇÕES
+
+# Atualizando repositório e fazendo atualização do sistema
+
+apt_update(){
+  sudo apt update && sudo apt dist-upgrade -y
+}
+
+# -------------------------------------------------------------------------------- #
+# -------------------------------TESTES E REQUISITOS----------------------------------------- #
+
+# Internet conectando?
+testes_internet(){
+if ! ping -c 1 8.8.8.8 -q &> /dev/null; then
+  echo -e "${VERMELHO}[ERROR] - Seu computador não tem conexão com a Internet. Verifique a rede.${SEM_COR}"
+  exit 1
+else
+  echo -e "${VERDE}[INFO] - Conexão com a Internet funcionando normalmente.${SEM_COR}"
+fi
+}
+
+# ------------------------------------------------------------------------------ #
+
+
+## Removendo travas eventuais do apt ##
+travas_apt(){
+  sudo rm /var/lib/dpkg/lock-frontend
+  sudo rm /var/cache/apt/archives/lock
+}
+
+## Adicionando/Confirmando arquitetura de 32 bits ##
+add_archi386(){
+sudo dpkg --add-architecture i386
+}
+## Atualizando o repositório ##
+just_apt_update(){
+sudo apt update -y
+}
+
+PROGRAMAS_PARA_INSTALAR=(
+  snapd
+  gparted
+  timeshift
+  gufw
+  vlc
+  code
+  git
+  wget
+  ubuntu-restricted-extras
+)
+
+# ---------------------------------------------------------------------- #
+
+## Download e instalaçao de programas externos ##
+
+# Instalar programas no apt
+echo -e "${VERDE}[INFO] - Instalando pacotes apt do repositório${SEM_COR}"
+
+for nome_do_programa in ${PROGRAMAS_PARA_INSTALAR[@]}; do
+  if ! dpkg -l | grep -q $nome_do_programa; then # Só instala se já não estiver instalado
+    sudo apt install "$nome_do_programa" -y
+  else
+    echo "[INSTALADO] - $nome_do_programa"
+  fi
+done
+
+}
